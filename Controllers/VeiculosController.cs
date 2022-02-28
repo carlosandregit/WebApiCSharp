36a0f50f39334ae4b666dc0cf7520be4bc8583a7
carlosandregit <66290422+carlosandregit@users.noreply.github.com>
2022-02-28 20:13:54 -0300

    Implementando o banco de dados

--- Controllers/VeiculosController.cs
+++ Controllers/VeiculosController.cs
@@ -13,42 +13,33 @@ namespace WebApiCSharp.Controllers
     public class VeiculosController : ApiController
     {
 
-         public static List<Veiculos> listaVeiculos = new List<Veiculos>();
 
-
-        [HttpGet]
-        [Route("api/veiculos/popular")]
-        public JObject Popular()
-        {
-            listaVeiculos.Add(new Veiculos(1, "FORD FIESTA", "1.0 MPI PERSONALITÉ SEDAN 4P", 2005, 2005, "Preto", 2, false, 25000, true));
-            listaVeiculos.Add(new Veiculos(2, "Kia Cerato", "1.6 EX3 SEDAN 16V 4P A", 2014, 2015, "Prata", 3, true, 35000, true));
-            listaVeiculos.Add(new Veiculos(6, "HYUNDAI HB20", "1.0 COMFORT 12V FLEX 4P", 2017, 2017, "Azul", 3, true, 43000, true));
-            listaVeiculos.Add(new Veiculos(12, "CHEVROLET PRISMA", "1.4 MPFI LT BV FLEX 4P", 2013, 2013, "Preto", 3, true, 31000, true));
-            listaVeiculos.Add(new Veiculos(27, "VOLKSVAGEN POLO", "1.0 200 TSI HIGHLINE", 2018, 2018, "Amarelo", 3, true, 66045, true));
-
-            var resultado = JObject.Parse("{resultado : \"populado\"}");
-            return resultado;
-        }
-
-        // GET api/veiculos
+        //GET api/veiculos
         public List<Veiculos> Get()
         {
+            var listaVeiculos = Veiculos.GetVeiculos();
+
             return listaVeiculos;
-           
         }
 
         // GET api/veiculos/5
         public Veiculos Get(int id)
         {
-            return listaVeiculos.Find(x => x.Id.Equals(id));
+            var veiculos = new  Veiculos();
+            veiculos.GetVeiculo(id);
+
+            return veiculos;
         }
 
         // POST api/veiculos
         public JObject Post([FromBody] Veiculos veiculo)
         {
             var resultado = "";
-            if (veiculo.Id == 0)
-                resultado = "{resultado: \"Id não pode ser nulo nem zero\"}";
+          
+            if(veiculo == null)
+                resultado = "{resultado: \"Faltou parametro no body\"}";
+            else if (veiculo.Id != 0)
+                resultado = "{resultado: \"Id deve ser zero\"}";
             else if (String.IsNullOrEmpty(veiculo.Marca))
                 resultado += "{resultado : \"Marca deve ser informada\"}";
             else if(veiculo.Valor == 0)
@@ -56,38 +47,35 @@ namespace WebApiCSharp.Controllers
 
             if (String.IsNullOrEmpty(resultado))
             {
-                listaVeiculos.Add(new Veiculos(veiculo.Id, veiculo.Marca, veiculo.Modelo, veiculo.Ano, veiculo.Fabricacao, veiculo.Cor, veiculo.Combustivel,
-                veiculo.Automatico, veiculo.Valor, veiculo.Ativo));
-
-                resultado = "{resultado: \"OK\"}";
+                var veiculoNovo = new Veiculos();
+                resultado = veiculo.Salvar(veiculo);
+                resultado = "{resultado: \""+ resultado +"\"}";
             }
            return JObject.Parse(resultado);
         }
 
         // PUT api/veiculos/5
-        public void Put(int id, [FromBody] Veiculos veiculo)
+        public JObject Put(int id, [FromBody] Veiculos veiculo)
         {
-            var vei = listaVeiculos.Single(x => x.Id.Equals(id));
+            var resultado = "";
 
-            vei.Marca = veiculo.Marca;
-            vei.Modelo = veiculo.Modelo;
-            vei.Ano = veiculo.Ano;
-            vei.Fabricacao = veiculo.Fabricacao;
-            vei.Cor = veiculo.Cor;
-            vei.Combustivel = veiculo.Combustivel;
-            vei.Automatico = veiculo.Automatico;
-            vei.Valor = veiculo.Valor;
-            vei.Ativo = veiculo.Ativo;
-           
+            var vei = new Veiculos();
+            veiculo.Id = id;
+            resultado = vei.Salvar(veiculo);
+            resultado = "{resultado: \"" + resultado + "\"}";
 
+            return JObject.Parse(resultado);
         }
 
+        //api/veiculos/id
+        public JObject Delete(int id)
+        {           
 
-        public void Delete(int id)
-        {
-            var item = listaVeiculos.FindIndex(x => x.Id.Equals(id));
-            listaVeiculos.RemoveAt(item);
-          
+            var vei = new Veiculos();
+            var resultado = vei.Excluir(id);
+            resultado = "{resultado: \"" + resultado + "\"}";
+
+            return JObject.Parse(resultado);
         }
     }
 }
--- Models/Veiculos.cs
+++ Models/Veiculos.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 
 namespace WebApiCSharp.Models
 {
@@ -19,6 +21,10 @@ namespace WebApiCSharp.Models
         public decimal Valor { get; set; }
         public bool Ativo { get; set; }
 
+        public Veiculos()
+        {
+
+        }
 
         public Veiculos(int id, string marca, string modelo, short ano, short fabricacao, string cor,
             byte combustivel, bool automatico, decimal valor, bool ativo)
@@ -34,6 +40,159 @@ namespace WebApiCSharp.Models
             Valor = valor;
             Ativo = ativo;
 
+        }
+        private readonly static string _conn = WebConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+        public static List<Veiculos> GetVeiculos()
+        {
+            var listaCarros = new List<Veiculos>();
+            var sql = "select * from Veiculos";
+
+            try
+            {
+                using (var cn = new SqlConnection(_conn))
+                {
+                    cn.Open();
+                    using (var cmd = new SqlCommand(sql, cn))
+                    {
+                        using (var dr = cmd.ExecuteReader())
+                        {
+                            if (dr.HasRows)
+                            {
+                                while (dr.Read())
+                                {
+                                    listaCarros.Add(new Veiculos(
+                                       Convert.ToInt32(dr["Id"]),
+                                        dr["Nome"].ToString(),
+                                        dr["Modelo"].ToString(),
+                                        Convert.ToInt16(dr["Ano"]),
+                                        Convert.ToInt16(dr["Fabricacao"]),
+                                        dr["Cor"].ToString(),
+                                        Convert.ToByte(dr["Combustivel"]),
+                                        Convert.ToBoolean(dr["Automatico"]),
+                                        Convert.ToDecimal(dr["Valor"]),
+                                        Convert.ToBoolean(dr["Ativo"])
+                                        ));
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Falha: " + ex.Message);
+            }
+            return listaCarros;
+        }
+
+        public string Salvar(Veiculos veiculo)
+        {
+            var sql = "";
+
+            if(veiculo.Id == 0)            
+                sql = "INSERT INTO Veiculos(nome,  modelo,  ano,  fabricacao,  cor, combustivel,  automatico,  valor,  ativo)" +
+                    "VALUES (@nome,  @modelo,  @ano,  @fabricacao,  @cor, @combustivel,  @automatico,  @valor,  @ativo)";
+
+  
+            else          
+                sql = "UPDATE Veiculos SET nome=@nome,  modelo=@modelo,  ano=@ano,  fabricacao=@fabricacao,  cor=@cor, combustivel=@combustivel,  automatico=@automatico,  valor=@valor,  ativo=@ativo " +
+                    " WHERE id = " + veiculo.Id;
+
+                try
+                {
+                    using (var cn = new SqlConnection(_conn))
+                    {
+                        cn.Open();
+                        using (var cmd = new SqlCommand(sql, cn))
+                        {
+
+                            cmd.Parameters.AddWithValue("@nome", veiculo.Marca);
+                            cmd.Parameters.AddWithValue("@modelo", veiculo.Modelo);
+                            cmd.Parameters.AddWithValue("@ano", veiculo.Ano);
+                            cmd.Parameters.AddWithValue("@fabricacao", veiculo.Fabricacao);
+                            cmd.Parameters.AddWithValue("@cor", veiculo.Cor);
+                            cmd.Parameters.AddWithValue("@combustivel", veiculo.Combustivel);
+                            cmd.Parameters.AddWithValue("@automatico", veiculo.Automatico);
+                            cmd.Parameters.AddWithValue("@valor", veiculo.Valor);
+                            cmd.Parameters.AddWithValue("@ativo", veiculo.Ativo);
+
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+
+                    return "ok";
+
+                }
+                catch (Exception ex)
+                {
+                    return "Falha: " + ex.Message;
+                }
+            
+        }
+
+        public string Excluir(int id)
+        {
+            var sql = "DELETE FROM Veiculos WHERE id = " + id;
+
+            try
+            {
+                using (var cn = new SqlConnection(_conn))
+                {
+                    cn.Open();
+                    using (var cmd = new SqlCommand(sql, cn))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                return "Excluido";
+            }
+            catch (Exception ex)
+            {
+               return "Falha: " + ex.Message;
+            }
+        }
+        public void GetVeiculo(int id)
+        {
+            var sql = "select nome,  modelo,  ano,  fabricacao,  cor, combustivel,  automatico,  valor,  ativo from Veiculos " +
+                "where id = " + id;
+
+            try
+            {
+                using (var cn = new SqlConnection(_conn))
+                {
+                    cn.Open();
+                    using (var cmd = new SqlCommand(sql, cn))
+                    {
+                        using (var dr = cmd.ExecuteReader())
+                        {
+                            if (dr.HasRows)
+                            {
+                                while (dr.Read())
+                                {
+
+                                    Id = id;
+                                    Marca = dr["Nome"].ToString();
+                                    Modelo = dr["Modelo"].ToString();
+                                    Ano = Convert.ToInt16(dr["Ano"]);
+                                    Fabricacao = Convert.ToInt16(dr["Fabricacao"]);
+                                    Cor = dr["Cor"].ToString();
+                                    Combustivel = Convert.ToByte(dr["Combustivel"]);
+                                    Automatico = Convert.ToBoolean(dr["Automatico"]);
+                                    Valor = Convert.ToDecimal(dr["Valor"]);
+                                    Ativo = Convert.ToBoolean(dr["Ativo"]);
+
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Falha: " + ex.Message);
+            }
         }
     }
 }
